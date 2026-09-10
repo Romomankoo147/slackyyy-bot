f661f1abbe9e07de13ec7887d49f1e62ea3afee5
Roman <romko.romomanko@gmail.com>
2026-09-10 20:48:57 +0200

    Forgot ;

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ app.command("/slackyyy-bot-flip", async ({ ack, respond}) => {
     await ack();
     const result = Math.random() < 0.5 ? "🪙 *Heads!*" : "🪙 *Tails!*";
     await respond ({ text: result });
-})
+});
 
 
 (async () => {
